bbc23da6290a5613d1de61f4317b5df54f20b933
Fernando Aparicio <faparicior@gmail.com>
2024-11-15 20:26:12 +0100

    Refactor user security and user domain

--- php/src/Advertisements/User/Application/Command/SignUpMember/SignUpMemberCommand.php
+++ php/src/Advertisements/User/Application/Command/SignUpMember/SignUpMemberCommand.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace Demo\App\Advertisements\User\Application\Command\SignUpMember;
 
+use SensitiveParameter;
+
 readonly class SignUpMemberCommand
 {
     public function __construct(
@@ -10,6 +12,7 @@ readonly class SignUpMemberCommand
         public string $userRole,
         public string $id,
         public string $email,
+        #[SensitiveParameter]
         public string $password,
         public string $memberNumber,
         public string $civicCenterId,
