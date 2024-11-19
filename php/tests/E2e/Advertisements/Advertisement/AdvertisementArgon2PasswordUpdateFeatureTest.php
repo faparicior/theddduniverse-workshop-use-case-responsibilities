af761f7215fb26b998f49ec199ebbf08608d86d6
Fernando Aparicio <faparicior@gmail.com>
2024-11-19 19:12:54 +0100

    Fix tests

--- php/tests/E2e/Advertisements/Advertisement/AdvertisementArgon2PasswordUpdateFeatureTest.php
+++ php/tests/E2e/Advertisements/Advertisement/AdvertisementArgon2PasswordUpdateFeatureTest.php
@@ -35,6 +35,8 @@ final class AdvertisementArgon2PasswordUpdateFeatureTest extends TestCase
 
     public function testShouldPublishAnAdvertisementWithArgon2Password(): void
     {
+        $this->withMemberUser('enabled');
+
         $request = new FrameworkRequest(
             FrameworkRequest::METHOD_POST,
             'advertisement',
@@ -45,6 +47,9 @@ final class AdvertisementArgon2PasswordUpdateFeatureTest extends TestCase
                 'password' => 'myPassword',
                 'memberId' => self::MEMBER_ID,
                 'civicCenterId' => self::CIVIC_CENTER_ID,
+            ],
+            [
+                'userSession' => self::MEMBER_ID,
             ]
         );
 
@@ -61,6 +66,7 @@ final class AdvertisementArgon2PasswordUpdateFeatureTest extends TestCase
 
     public function testShouldChangeToArgon2PasswordUpdatingAnAdvertisement(): void
     {
+        $this->withMemberUser('enabled');
         $this->withAnAdvertisementWithAMd5PasswordCreated();
 
         $request = new FrameworkRequest(
@@ -71,6 +77,9 @@ final class AdvertisementArgon2PasswordUpdateFeatureTest extends TestCase
                 'email' => 'email@test.com',
                 'password' => 'myPassword',
             ],
+            [
+                'userSession' => self::MEMBER_ID,
+            ]
         );
 
         $response = $this->server->route($request);
@@ -94,6 +103,9 @@ final class AdvertisementArgon2PasswordUpdateFeatureTest extends TestCase
             [
                 'password' => 'myPassword',
             ],
+            [
+                'userSession' => self::MEMBER_ID,
+            ]
         );
 
         $response = $this->server->route($request);
@@ -107,11 +119,26 @@ final class AdvertisementArgon2PasswordUpdateFeatureTest extends TestCase
         $this->expectHasAnArgon2Password($resultSet[0]['password']);
     }
 
+
     private function emptyDatabase(): void
     {
         $this->connection->execute('delete from advertisements;');
+        $this->connection->execute('delete from users;');
     }
 
+    private function withMemberUser(string $status): void
+    {
+        $this->connection->execute(sprintf("INSERT INTO users (id, email, password, role, member_number, civic_center_id, status) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s')",
+                self::MEMBER_ID,
+                'member@test.com',
+                md5('myPassword'),
+                'member',
+                '123456',
+                self::CIVIC_CENTER_ID,
+                $status,
+            )
+        );
+    }
     private function withAnAdvertisementWithAMd5PasswordCreated(): void
     {
         $this->connection->execute(sprintf("INSERT INTO advertisements (id, description, email, password, advertisement_date, status, approval_status, user_id, civic_center_id) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s' )",
